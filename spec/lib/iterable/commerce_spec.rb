f612b912294e4786e390938ed243538d9ece7601
Chavez <matthew@el-chavez.me>
2017-10-14 20:09:40 -0700

    Update user shopping cart items
    - Added Iterable::Commerce#update_cart

--- spec/lib/iterable/commerce_spec.rb
+++ spec/lib/iterable/commerce_spec.rb
@@ -48,4 +48,50 @@ RSpec.describe Iterable::Commerce, :vcr do
       end
     end
   end
+
+  describe 'update_cart' do
+    let(:commerce_item) do
+      {
+        id: SecureRandom.uuid,
+        name: 'Sample Item',
+        price: 100.0,
+        quantity: 1
+      }
+    end
+    let(:items) { [commerce_item] }
+    let(:user) do
+      {
+        userId: '1',
+        email: 'sample@example.com'
+      }
+    end
+    let(:res) { subject.update_cart user, items }
+
+    context 'successfully' do
+      it 'responds with success' do
+        expect(res).to be_success
+      end
+
+      it 'responds with response object' do
+        expect(res).to be_a(Iterable::Response)
+      end
+
+      it 'returns success code' do
+        expect(res.body['code']).to match(/success/i)
+      end
+    end
+
+    context 'without required params' do
+      let(:user) { {} }
+      let(:items) { [] }
+
+      it 'is not successful' do
+        expect(res).not_to be_success
+      end
+
+      it 'responds with an error code' do
+        expect(res.code).to eq('400')
+      end
+    end
+  end
 end
